f9a341c7abe78967727341b833acdbf5509d56ed
bradsacks99 <56355677+bradsacks99@users.noreply.github.com>
2021-04-19 22:09:58 -0700

    Update set_registry.sh

--- bin/set_registry.sh
+++ bin/set_registry.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl create secret docker-registry squall-registry --docker-server=docker.io --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_TOKEN --docker-email=bradsacks99@gmail.com
+kubectl create secret docker-registry squall-registry --docker-server=docker.io --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_TOKEN --docker-email=$DOCKER_EMAIL
